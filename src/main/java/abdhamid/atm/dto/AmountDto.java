159be8471e8c8a8e8fb68f4ec7fd2e5eb2875e2d
Abdul Hamid Umar <AbdulHamid.Umar@mitrais.com>
2022-08-09 08:35:01 +0700

    add dto

---
+++ src/main/java/abdhamid/atm/dto/AmountDto.java
@@ -0,0 +1,14 @@
+package abdhamid.atm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AmountDto {
+    private Integer amount;
+}
---
+++ src/main/java/abdhamid/atm/dto/TransferDto.java
@@ -0,0 +1,15 @@
+package abdhamid.atm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransferDto {
+    private String accNumber;
+    private Integer amount;
+}
